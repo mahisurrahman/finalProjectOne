44b272b4b1dd4b8db424b84be569bfb50c2d4c2b
mahisurrahman <zordanblog009@gmail.com>
2023-11-18 05:40:08 +0600

    Added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,14 +5,59 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
+
 const Login = () => {
     return (
-        <div className="loginDiv max-w-full h-screen px-5 md:px-20 py-5 md:py-10 font-inter-font">
-            <div className='loginDivTwo h-full border-2 shadow-lg shadow-gray-400 grid grid-cols-1 md:grid-cols-2 px-5 md:px-10 py-2 md:py-5 lg:py-2'>
-                <div className='hidden md:flex items-center'>
-                    <img src={loginImage} className='md:w-96 lg:w-full' alt="" />
+        <div className="loginDiv max-w-full h-screen px-20 py-10 font-inter-font">
+            <div className='loginDivTwo h-full border-2 shadow-xl shadow-gray-400 grid grid-cols-2 px-10 py-5'>
+                <div className='flex items-center'>
+                    <img src={loginImage} className='w-96' alt="" />
                 </div>
-                <div className='text-center py-10 md:py-0 lg:py-0'>
+                <div>
+                    <div>
+                        <h2 className='text-center text-2xl font-bold'>Login</h2>
+                    </div>
+                    <form>
+                        <div className="mb-2">
+                            <label htmlFor="email" className="block text-sm font-medium text-black dark:text-black">Your email</label>
+                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type Here" required/>
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="password" className="block text-sm font-medium text-black dark:text-black">Your password</label>
+                            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Enter Your Password Here' required/>
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="captchaDisplay" className="block text-sm font-medium text-black dark:text-black">Captcha</label>
+                            <input type="text" id="captchaDisplay" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                            <a className='text-xs font-semibold text-blue-800 tracking-widest' href="">Reload Captcha</a>
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="captchaInput" className="block text-sm font-medium text-blue dark:text-black"></label>
+                            <input type="text" id="captchaInput" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Enter The Above Captcha Here' required/>
+                        </div>
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#d1a054b3] dark:hover:bg-black dark:hover:text-[#d1a054b3] dark:hover:duration-700 dark:focus:ring-blue-800">Sign In</button>
+                    </form>
+                    <div className='my-2 flex items-center justify-center gap-1'>
+                        <h2 className='text-md text-[#d1a054b3]'>New Here?</h2>
+                        <NavLink><button className='text-md text-[#d1a054b3] font-bold hover:text-black hover:duration-700'>Create a New Account</button></NavLink>
+                    </div>
+                    <div className='mt-8'>
+                        <h2 className='text-sm text-black text-center font-bold tracking-widest'>Or Sign In With</h2>
+                        <div className='mt-2 flex justify-center gap-10'>
+                            <NavLink><button className='border rounded-full border-gray-700 px-2 py-2 hover:bg-black hover:text-white hover:duration-700 hover:cursor-pointer'><FaFacebookF></FaFacebookF></button></NavLink>
+                            <NavLink><button className='border rounded-full border-gray-700 px-2 py-2 hover:bg-black hover:text-white hover:duration-700 hover:cursor-pointer'><FaGoogle></FaGoogle></button></NavLink>
+                            <NavLink><button className='border rounded-full border-gray-700 px-2 py-2 hover:bg-black hover:text-white hover:duration-700 hover:cursor-pointer'><FaGithub></FaGithub></button></NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
+
+{/* <div className='text-center py-10 md:py-0 lg:py-0'>
                     <h2 className="text-xl md:text-xl lg:text-2xl font-bold uppercase">Login</h2>
                     <div className='px-2 md:px-20'>
                         <form className='text-left' action="">
@@ -48,10 +93,4 @@ const Login = () => {
                             <NavLink><button className='border rounded-full border-black px-2 py-2 hover:bg-black hover:text-white hover:duration-700 hover:cursor-pointer'><FaGithub></FaGithub></button></NavLink>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    );
-};
-
-export default Login;
+                </div> */}
