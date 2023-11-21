f5124b4e8d869021e50f2d02f45328818433abdb
mahisurrahman <zordanblog009@gmail.com>
2023-11-22 03:01:24 +0600

    Added

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -1,9 +1,38 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Swal from 'sweetalert2';
+import axios from "axios";
+
+const FoodCard = ({ item }) => {
+    const {name, recipe, image, price} = item;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { user } = useAuth();
 
 
-const FoodCard = ({item}) => {
-    const handleAddToCart = food =>{
-        console.log(food);
+    const handleAddToCart = food => {
+        if(user && user.email){
+            const cartItem = {
+                menuId: food._id,
+                email: user.email,
+                name,
+                image, 
+                price,
+                recipe,
+            }
+            axios.post('http://localhost:5000/carts', cartItem)
+            .then(result=>{
+                console.log(result.data);
+                if(result.data.insertedId){
+                    Swal.fire("Successfully Added to Cart");
+                }
+            })
+        }else{
+            Swal.fire("Please Sign In First!");
+            navigate('/login', {state: {from:location}});
+        }
     }
+
     return (
         <div>
             <img src={item.image} alt="" />
@@ -11,9 +40,9 @@ const FoodCard = ({item}) => {
                 <h2 className='text-xl text-center pt-5 font-bold'>{item.name}</h2>
                 <p className='text-sm text-center pt-1'>{item.recipe}</p>
                 <div className='flex justify-center pt-5 pb-7'>
-                    <button  
-                    onClick={()=>handleAddToCart(item)}
-                    className='px-4 py-2 bg-base-300 rounded-lg border-b-2 border-yellow-600 uppercase hover:bg-black hover:text-white hover:duration-700'>Add to Cart</button>
+                    <button
+                        onClick={() => handleAddToCart(item)}
+                        className='px-4 py-2 bg-base-300 rounded-lg border-b-2 border-yellow-600 uppercase hover:bg-black hover:text-white hover:duration-700'>Add to Cart</button>
                 </div>
             </div>
         </div>
